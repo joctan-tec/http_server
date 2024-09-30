f09339741d22b156f44a25c49ef2e0cc7c784e39
joctan04 <joctan@estudiantec.cr>
2024-09-30 00:34:04 -0600

    archivo json corregido, main sin thread pool

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,9 +3,8 @@ use std::fs;
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
 use serde::{Deserialize, Serialize};
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::sync::mpsc::{self, Sender, Receiver};
+mod thread_pool;
+use thread_pool::ThreadPool;
 use std::process::{Command, Output};
 use std::env;
 use serde_json::{json, Value};
@@ -369,72 +368,6 @@ fn handle_connection(mut stream: TcpStream) {
 
     if let Err(e) = stream.write_all(response.as_bytes()) {
         eprintln!("Failed to write response: {}", e);
-    }
-}
-
-/// Definición del ThreadPool y sus componentes
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: Sender<Job>,
-}
-
-type Job = Box<dyn FnOnce() + Send + 'static>;
-
-impl ThreadPool {
-    /// Crea un nuevo ThreadPool con el tamaño especificado
-    pub fn new(size: usize) -> ThreadPool {
-        let (sender, receiver) = mpsc::channel();
-        let receiver = Arc::new(Mutex::new(receiver));
-
-        let mut workers = Vec::with_capacity(size);
-        for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
-        }
-
-        ThreadPool { workers, sender }
-    }
-
-    /// Ejecuta una nueva tarea en el ThreadPool
-    pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
-        let job = Box::new(f);
-        self.sender.send(job).expect("Failed to send job to the thread pool");
-    }
-}
-
-struct Worker {
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
-}
-
-impl Worker {
-    /// Crea un nuevo Worker y lo añade al ThreadPool
-    fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {} got a job; executing.", id);
-            job();
-        });
-
-        Worker {
-            id,
-            thread: Some(thread),
-        }
-    }
-}
-
-impl Drop for ThreadPool {
-    /// Espera a que todos los hilos finalicen al destruir el ThreadPool
-    fn drop(&mut self) {
-        // Se deja de enviar trabajos al sender
-        drop(self.sender.clone());
-        for worker in &mut self.workers {
-            if let Some(thread) = worker.thread.take() {
-                thread.join().unwrap();
-            }
-        }
     }
 }
 
